7ec0703cc2dd967a19ee11b45fcd2851812dd098
nabeelktr <nabee446@gmail.com>
2024-04-20 10:45:50 +0530

    added recieve message fn in socket

--- src/modules/admin/route.ts
+++ src/modules/admin/route.ts
@@ -9,7 +9,7 @@ adminRoute.get("/get-users", isValidated, controller.getAllUsers);
 adminRoute.get("/get-instructors", isValidated, controller.getAllInstructors);
 adminRoute.delete("/delete-user/:id", isValidated, controller.deleteUser);
 adminRoute.post("/add-faq", isValidated, controller.addFAQ);
-adminRoute.get("/get-faq", isValidated, controller.getFAQ);
+adminRoute.get("/get-faq", controller.getFAQ);
 adminRoute.post("/add-categories", isValidated, controller.addCategories);
 adminRoute.get("/get-categories", controller.getCategories);
 
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,17 +1,31 @@
-import http from 'http'
-import {Server as SockerIoServer} from 'socket.io'
+import http from "http";
+import { Server as SockerIoServer } from "socket.io";
+import { UserClient } from "../modules/user/config/grpc-client/user.client";
 
 export const initSocketServer = (server: http.Server) => {
-    const io= new SockerIoServer(server);
-    io.on("connection", (socket) => {
-        console.log("user connected");
+  const io = new SockerIoServer(server);
+  io.on("connection", (socket) => {
+    console.log("user connected");
 
-        socket.on("notification", (data) => {
-            io.emit("newNotification", data)
-        })
+    socket.on("notification", (data) => {
+      io.emit("newNotification", data);
+    });
 
-        socket.on("disconnect", () => {
-            console.log("user disconnected");
-        })
-    })
-}
+    socket.on("startStream", (data) => {
+      UserClient.GetUser({ id: data.instructorId }, (err, result) => {
+        io.emit("joinStream", {
+          courses: result?.courses,
+          streamId: data.callid,
+        });
+      });
+    });
+
+    socket.on("sendMessage", (datas) => {
+      io.emit("recieveMessage", datas);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+};
